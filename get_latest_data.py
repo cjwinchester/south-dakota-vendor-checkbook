04c8eb74af7b9aeab83893fa543ea567bbe89f63
cjwinchester <cjwinchester@gmail.com>
2024-11-08 23:15:07 -0700

    fixed

--- get_latest_data.py
+++ get_latest_data.py
@@ -1,6 +1,6 @@
-import datetime
+from datetime import date
 import os
-from glob import glob
+from pathlib import Path
 
 import pandas as pd
 
@@ -21,92 +21,131 @@ CSV_READ_SETTINGS = {
     ],
     'encoding': 'utf-8',
     'dtype': {
+        'agency_code': str,
         'agency': str
-    }
+    },
+    'low_memory': False
 }
 
-DATA_DIR = 'data'
+DATA_DIR = Path('data')
+
+TODAY = date.today().strftime('%B %-d, %Y')
+
+
+def build_readme(metadata={}):
+    record_count = metadata.get('record_count')
+    start_date = metadata.get('start_date')
+    end_date = metadata.get('end_date')
+    
+    with open('readme-template.md', 'r') as infile:
+        tmpl = infile.read()
+
+    replacements = {
+        '{% UPDATED %}': TODAY,
+        '{% RECORD_COUNT %}': record_count,
+        '{% START_DATE %}': start_date,
+        '{% END_DATE %}': end_date,
+    }
+
+    for key in replacements:
+        tmpl = tmpl.replace(key, replacements[key])
+
+    with open('README.md', 'w') as outfile:
+        outfile.write(tmpl)
+
+    print('Wrote README.md')
 
 
 def get_latest():
 
-    this_year = datetime.date.today().year
     base_url = 'http://bfm.sd.gov/ledger/CheckbookDetail'
 
-    urls = {
-        this_year: f'{base_url}CurrentYear.csv',
-        this_year-1: f'{base_url}PriorYear1.csv',
-        this_year-2: f'{base_url}PriorYear2.csv'
-    }
+    urls = [
+        f'{base_url}CurrentYear.csv',
+        f'{base_url}PriorYear1.csv',
+        f'{base_url}PriorYear2.csv'
+    ]
 
     print('Fetching data from state website ...')
 
+    df_codes = pd.read_csv(
+        'sd-agency-codes.csv',
+        dtype={
+            'agency_code': str
+        }
+    )
+
     # read data from three remote files into a df
-    df_new = pd.concat([pd.read_csv(urls[x], **CSV_READ_SETTINGS) for x in urls])  # noqa
-
-    # read existing data from local data directory
-    df_existing = pd.concat([pd.read_csv(x, **CSV_READ_SETTINGS) for x in glob(f'{DATA_DIR}/*.csv')])  # noqa
-
-    print(f'New data from website: {len(df_new):,}')
-    print(f'Existing record count: {len(df_existing):,}')
-
-    # merge the two data frames
-    df = pd.concat([df_existing, df_new])
+    df_new = pd.concat([pd.read_csv(x, **CSV_READ_SETTINGS) for x in urls])  # noqa
 
     # per 2022-12-02 email from a state accountant,
     # 032 used to be ag and 20 was environment, and
     # the combined agency is now DANR, 03
     def fix_danr_codes(row):
-        if row['agency'] in ['032', '20']:
+        if row['agency_code'] in ['032', '20']:
             return '03'
         else:
-            return row['agency']
+            return row['agency_code']
 
-    df['agency'] = df.apply(fix_danr_codes, axis=1)
-
-    # drop duplicates
-    df.drop_duplicates(inplace=True)
-
-    # sort by payment date
-    df.sort_values(
-        'ap_payment_date',
-        ascending=False,
+    df_new.rename(
+        columns={'agency': 'agency_code'},
         inplace=True
     )
 
-    # read in the agency code data
-    df_codes = pd.read_csv(
-        'sd-agency-codes.csv',
-        dtype={'agency_code': str}
-    )
+    df_new['agency_code'] = df_new.apply(fix_danr_codes, axis=1)
 
-    # merge into dataframe
-    df = pd.merge(
-        df,
+    df_new = pd.merge(
+        df_new,
         df_codes,
         how='left',
-        left_on='agency',
-        right_on='agency_code'
+        on='agency_code'
+    )
+
+    # read existing data from local files
+    df_existing = pd.concat([pd.read_csv(x, **CSV_READ_SETTINGS) for x in DATA_DIR.glob('*.csv')])  # noqa
+
+    print(f'Data from website: {len(df_new):,}')
+    print(f'Existing record count: {len(df_existing):,}')
+
+    df_combo = pd.concat([df_existing, df_new])
+
+    # drop duplicates
+    df_combo.drop_duplicates(inplace=True)
+
+    # check for missing agency names
+    missing_agency = df_combo[df_combo['agency_name'] == '']
+
+    if len(missing_agency) > 0:
+        print(df_combo)
+        raise Exception('Missing agency name(s)')
+
+    print(f'Combined, deduplicated data: {len(df_combo):,}')
+
+    # sort by payment date and vendor name
+    df_combo.sort_values(
+        ['ap_payment_date', 'vendor_name'],
+        inplace=True
     )
 
     # add a new column to group by
-    df['monthyear'] = df['ap_payment_date'].dt.strftime('%Y%m')
-
-    new_count = 0
+    df_combo['monthyear'] = df_combo['ap_payment_date'].dt.strftime('%Y%m')
 
     # write out monthly files
-    for monthyear, month_df in df.groupby('monthyear'):
-        filepath = os.path.join(
-            'data',
-            f'{monthyear}.csv'
-        )
-        new_count += len(month_df)
-        month_df_cols = [x for x in month_df.columns if x not in ['monthyear', 'agency']]  # noqa
+    for monthyear, month_df in df_combo.groupby('monthyear'):
+        filepath = DATA_DIR / f'{monthyear}.csv'
+
+        month_df_cols = [x for x in month_df.columns if x != 'monthyear']  # noqa
 
         month_df[month_df_cols].to_csv(filepath, index=False)
         print(f'Wrote {filepath}')
 
-    print(f'New record count: {new_count:,}')
+    metadata = {
+        'record_count': f'{len(df_combo):,}',
+        'start_date': df_combo['ap_payment_date'].min().strftime('%B %-d, %Y'),
+        'end_date': df_combo['ap_payment_date'].max().strftime('%B %-d, %Y'),
+    }
+
+    build_readme(metadata=metadata)
 
 
 if __name__ == '__main__':
---
+++ readme-template.md
@@ -0,0 +1,4 @@
+# South Dakota vendor checkbook data
+_Updated {% UPDATED %}_
+
+[Archiving](get_latest_data.py) South Dakota vendor checkbook data in [monthly CSV files](data) -- {% RECORD_COUNT %} records covering {% START_DATE %}, to {% END_DATE %} -- and [a Jupyter notebook](Analyze%20checkbook%20data.ipynb) to poke at the data a little.
